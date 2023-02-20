dc49b4e408a88d7e969a9fa6d75bbd7f9950bb04
Nestor Juarez <nesk.company@gmail.com>
2023-02-19 19:08:37 -0600

    Configuracion de redireccion a Index

--- rentasapp/views.py
+++ rentasapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    "Vista para mostar pagina principal"
+    return render(request, "rentasapp/index.html")
